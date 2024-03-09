3e964c6b9bfd832d3bcb0767737ea5a0824fe82d
Pranjali Dhale <pranjalidhale@Pranjalis-MacBook-Pro.local>
2024-03-09 10:26:17 +0530

    third commit

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 
-let rndno1=-1;
-let rndno2=-1;
-let img1=document.querySelector(".img1")
-img1.addEventListener('click',()=>{
-    rndno1=Math.floor(Math.random() * 6)+1;
-// var audio=new Audio('mixkit-arcade-retro-game-over-213.wav')
-// audio.play();
-    console.log(rndno1);
-    img1.src=`images/dice${rndno1}.png`;
-    let title=document.querySelector("h1");
-if(rndno1>rndno2)
-{
-    title.textContent='Player 1 is winner ';
-}
-if(rndno1<rndno2)
-{
-    title.textContent='Player 2 is winner';
-}
-if(rndno1==rndno2)
-{
-    title.textContent='Draw';
-}
-});
+// let rndno1=-1;
+// let rndno2=-1;
+// let img1=document.querySelector(".img1")
+// img1.addEventListener('click',()=>{
+//     rndno1=Math.floor(Math.random() * 6)+1;
+// // var audio=new Audio('mixkit-arcade-retro-game-over-213.wav')
+// // audio.play();
+//     console.log(rndno1);
+//     img1.src=`images/dice${rndno1}.png`;
+//     let title=document.querySelector("h1");
+// if(rndno1>rndno2)
+// {
+//     title.textContent='Player 1 is winner ';
+// }
+// if(rndno1<rndno2)
+// {
+//     title.textContent='Player 2 is winner';
+// }
+// if(rndno1==rndno2)
+// {
+//     title.textContent='Draw';
+// }
+// });
 
 
 let img2=document.querySelector(".img2")
